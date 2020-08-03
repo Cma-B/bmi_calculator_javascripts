1e9879490e074d83901b0e5311c56aa146c940c1
Sima <sima.boreiri@gmail.com>
2020-08-03 09:50:24 +0200

    adds two new lines of codes for weight and height and add input fields for in html file to pass the test

--- cypress/integration/userCanCheckTheBmi.feature.js
+++ cypress/integration/userCanCheckTheBmi.feature.js
@@ -2,5 +2,7 @@ describe("user can check the BMI", () => {
   it("user can enter weight and height", () => {
     cy.visit("http://localhost:3001")
     cy.contains("BMI Calculator")
+    cy.get("#weight").type('65')
+    cy.get("#height").type('165')
   })
 })
